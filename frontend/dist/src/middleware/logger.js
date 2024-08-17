6999ae4c215b9eb4822638b66fb1677de8a7b88f
Daniel Hernandez Puerto <sirdanielsan64@gmail.com>
2024-08-17 17:32:55 +0200

    Clean project

--- frontend/dist/src/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function logger(req, res, next) {
-    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
-    next();
-}
-exports.default = logger;
